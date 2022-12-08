aa67d9f83649fe560ed6db64e040b0b3ce9c20cd
PhucnguyenHoai <knkinhu@gmail.com>
2022-12-08 23:09:50 +0700

    add git config

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -15,5 +15,6 @@ INSERT INTO PROXY_TBL (address, port, protocol, status, user_id) VALUES ('192.16
 --cf login -u knkinhu@gmail.com -p phuc55
 --cf push example -p build/libs/example-1.0-SNAPSHOT.jar
 --cf push example -p /Users/nguyenphuc/Downloads/example/build/libs/example-1.0-SNAPSHOT.jar -b https://github.com/cloudfoundry/java-buildpack.git
---
+--git config --global user.name "your_github_username"
+--  $ git config --global user.email "your_github_email" git config -l
 --cf set-env example JBP_CONFIG_SPRING_AUTO_RECONFIGURATION: '{enabled: false}'
